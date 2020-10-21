12b6a570a800c72752f53d99571b95edede32314
Gorgious56 <nathan.hild@gmail.com>
2020-10-21 19:39:16 +0200

    Really fix the Point Density node this time

--- material_to_script.py
+++ material_to_script.py
@@ -15,10 +15,9 @@ from bpy.types import (
     NodeSocketVectorTranslation,
     NodeSocketVectorEuler,
     NodeSocketColor,
-    
+
     NodeReroute,
-    NodeFrame,
-    
+
     Object,
     Image,
     ImageUser,
@@ -26,11 +25,12 @@ from bpy.types import (
     ParticleSystem,
     CurveMapping,
     ColorRamp,
-    
+
     ShaderNodeTree,
-    )
+)
 
 from mathutils import Vector, Color
+
 
 def get_link_statement(link):
     """
@@ -40,28 +40,34 @@ def get_link_statement(link):
 links.new({link.from_node.path_from_id()}.outputs[{get_socket_index(link.from_socket)}]\
 , {link.to_node.path_from_id()}.inputs[{get_socket_index(link.to_socket)}])\
     """
-    
+
+
 def value_from_socket(socket):
     """
     Returns the evaluated value of a node socket's default value
     """
-    # A Shader socket (green dot) doesn't have a default value
+    # A Shader socket (green dot) doesn't have a default value :
     if isinstance(socket, NodeSocketShader):
         return NodeSocketShader.__name__
-    elif isinstance(socket, (NodeSocketVector, NodeSocketVectorXYZ, NodeSocketVectorTranslation, NodeSocketVectorEuler, NodeSocketVectorDirection)):
-        dv = socket.default_value
-        return f"({dv[0]}, {dv[1]}, {dv[2]})"
+    elif isinstance(socket, (
+            NodeSocketVector,
+            NodeSocketVectorXYZ,
+            NodeSocketVectorTranslation,
+            NodeSocketVectorEuler,
+            NodeSocketVectorDirection)):
+        return f"{[socket.default_value[i] for i in range(3)]}"
     elif isinstance(socket, NodeSocketColor):
-        dv = socket.default_value
-        return f"({dv[0]}, {dv[1]}, {dv[2]}, {dv[3]})"
+        return f"{[socket.default_value[i] for i in range(4)]}"
     else:
         return socket.default_value.__str__()
 
+
 class NodeCreator:
     """
-    Helper class to build statements to programmatically recreate the passed node
+    Helper class to programmatically recreate the passed node
     """
-    # These props are internal or read-only and aren't useful in the serialization.
+    # These props are internal or read-only
+    # and aren't useful in the serialization.
     default_props = (
         "dimensions",
         "draw_buttons",
@@ -80,54 +86,55 @@ class NodeCreator:
         "type",
         "update",
         "viewLocation",
-        
+
         "texture_mapping",
         "color_mapping",
-        
+
         "filepath",
-        
+
         "cache_point_density",
         "calc_point_density",
         "calc_point_density_minmax",
-        
-        "interface",         
-        
-        "height",  
+
+        "interface",
+
+        "height",
         "show_options",
         "show_preview",
         "show_texture",
         "width_hidden",
     )
-    
+
     def __init__(self, node):
         """
-        Initialize the node inputs and outputs, and the different fields' default values
+        Initialize the node inputs and outputs,
+        and the different fields' default values
         """
         self.node = node
         self.input_default_values = []
         self.output_default_values = []
         if not isinstance(node, NodeReroute):
-            for input in node.inputs:
-                self.input_default_values.append(value_from_socket(input))
+            for _input in node.inputs:
+                self.input_default_values.append(value_from_socket(_input))
             for output in node.outputs:
                 self.output_default_values.append(value_from_socket(output))
-        
+
         self.type = type(node).__name__
-        self.properties = [] # Could use an ordered dict instead.
+        self.properties = []  # Could use an ordered dict instead.
         for prop_name in dir(node):
             if prop_name.startswith("_") or prop_name.startswith("bl_"):
                 continue
             if prop_name in NodeCreator.default_props:
                 continue
             self.properties.append((prop_name, getattr(node, prop_name)))
-    
+
     def statements(self):
         """
         Build the chain of statements to programmatically recreate the node
         """
         statements = []
         statements.append(f"new_node = nodes.new(type='{self.type}')")
-        self.properties = sorted(self.properties, key=lambda p: p[0])                
+        self.properties = sorted(self.properties, key=lambda p: p[0])
         for props_tuple in self.properties:
             prop, value = props_tuple
             if isinstance(value, ImageUser):
@@ -143,14 +150,10 @@ img_text.tile = {value.tile}\
                 """)
                 continue
             if isinstance(value, ParticleSystem):
-                # /!\ Make sure this is executed after the node.object statement
-                print(value.name)            
+                # /!\ Make sure this is executed after node.object statement
                 statements.append(f"""\
-obj = new_node.object             
-if new_node.object:           
-    ps_mod = obj.modifiers.get({value.name})   
-    if ps_mod:
-        new_node.{prop} = ps_mod.particle_system\
+if new_node.object:
+    new_node.{prop} = new_node.object.particle_systems.get('{value.name}')
                 """)
                 continue
             if isinstance(value, CurveMapping):
@@ -164,10 +167,11 @@ map.tone = '{value.tone}'
 map.use_clip = {value.use_clip}\
                 """)
                 # Remove the 2 starting default points and only these :
-                for i, c in enumerate(value.curves):
-                    statements.append(f"map_c = map.curves[{i}]")   
-                    for p in c.points:
-                        statements.append(f"map_c.points.new({p.location[0]}, {p.location[1]})") 
+                for i, curve in enumerate(value.curves):
+                    statements.append(f"map_c = map.curves[{i}]")
+                    for point in curve.points:
+                        statements.append(f"""\
+map_c.points.new({point.location[0]}, {point.location[1]})""")
                     statements.append("""\
 removed_start = removed_end = False
 for i in range(len(map_c.points) - 1, -1, -1):
@@ -179,7 +183,7 @@ for i in range(len(map_c.points) - 1, -1, -1):
         map_c.points.remove(p)
         removed_end = True\
                     """)
-                statements.append(f"map.update()")                
+                statements.append(f"map.update()")
                 continue
             if isinstance(value, ColorRamp):
                 statements.append(f"""\
@@ -204,7 +208,7 @@ for i in range(len(cr.elements) - 1, -1, -1):
         removed_white = True\
                 """)
                 continue
-            if isinstance(value, ShaderNodeTree):                
+            if isinstance(value, ShaderNodeTree):
                 statements.append(f"""\
 ng = bpy.data.node_groups.get('{value.name}')
 if not ng:
@@ -218,7 +222,7 @@ else:
                 if value:
                     statements.append(f"new_node.{prop} = {value}")
             elif prop == "text" and not value:
-                continue      
+                continue
             elif prop in ("select", "shrink"):
                 if not value:
                     statements.append(f"new_node.{prop} = {value}")
@@ -227,58 +231,70 @@ else:
                     statements.append(f"new_node.{prop} = '{value}'")
             elif isinstance(value, Vector):
                 if len(value) == 2:
-                    statements.append(f"new_node.{prop} = ({value[0]}, {value[1]})") 
+                    statements.append(
+                        f"new_node.{prop} = ({value[0]}, {value[1]})")
                 else:
-                    statements.append(f"new_node.{prop} = ({value[0]}, {value[1]}, {value[2]})")    
+                    statements.append(
+                        f"new_node.{prop} = ({value[0]}, {value[1]}, {value[2]})")
             elif isinstance(value, Object):
-                statements.append(f"new_node.{prop} = bpy.data.objects.get('{value.name}')")
+                statements.append(
+                    f"new_node.{prop} = bpy.data.objects.get('{value.name}')")
             elif isinstance(value, Image):
-                statements.append(f"new_node.{prop} = bpy.data.images.get('{value.name}')")
+                statements.append(
+                    f"new_node.{prop} = bpy.data.images.get('{value.name}')")
             elif isinstance(value, Text):
                 if value:
-                    statements.append(f"new_node.{prop} = bpy.data.texts.get('{value.name}')")
+                    statements.append(
+                        f"new_node.{prop} = bpy.data.texts.get('{value.name}')")
             elif prop == "parent":
                 if value:
                     statements.append(f"""\
-parent = nodes.get('{value.name}')                    
+parent = nodes.get('{value.name}')
 if parent:
     new_node.parent = parent
     while True:
-        new_node.location += parent.location 
+        new_node.location += parent.location
         if parent.parent:
             parent = parent.parent
         else:
             break\
                     """)
             elif isinstance(value, Color):
-                statements.append(f"new_node.{prop} = ({value[0]}, {value[1]}, {value[2]})")
+                statements.append(
+                    f"new_node.{prop} = ({value[0]}, {value[1]}, {value[2]})")
             else:
                 statements.append(f"new_node.{prop} = {value}")
         for i, dv in enumerate(self.input_default_values):
             if dv == NodeSocketShader.__name__:
                 continue
             statements.append(f"new_node.inputs[{i}].default_value = {dv}")
-        
+
         for i, dv in enumerate(self.output_default_values):
             if dv == NodeSocketShader.__name__:
                 continue
             statements.append(f"new_node.outputs[{i}].default_value = {dv}")
-        
+
         if not isinstance(self.node, NodeReroute):
-            for input in self.node.inputs:
-                if input.hide:
-                    statements.append(f"new_node.inputs[{get_socket_index(input)}].hide = True")                
+            for _input in self.node.inputs:
+                if _input.hide:
+                    statements.append(
+                        f"new_node.inputs[{get_socket_index(_input)}].hide = True")
             for output in self.node.outputs:
                 if output.hide:
-                    statements.append(f"new_node.outputs[{get_socket_index(output)}].hide = True")
+                    statements.append(
+                        f"new_node.outputs[{get_socket_index(output)}].hide = True")
 #        DEBUG Print node location as a label :
 #        statements.append("new_node.label = str(new_node.location[0]).split('.')[0] + ', ' + str(new_node.location[1]).split('.')[0]")
 
         return statements
-        
+
 
 def serialize_material(mat):
-    nt = mat.node_tree
+    """
+    Returns the ordered statements necessary to build the 
+    Mateiral generation script
+    """
+    node_tree = mat.node_tree
     statements = [f"""\
 import bpy
 new_mat = bpy.data.materials.get('{mat.name}')
@@ -293,30 +309,33 @@ nodes.clear()
 links = node_tree.links
 links.clear()
     """]
-    
-    node_names = {}
-    
+
     statements.append("# Nodes :\n")
-    for node in nt.nodes:
+    for node in node_tree.nodes:
         for st in NodeCreator(node).statements():
             statements.append(st)
         statements.append("")
-        
-    if nt.links:    
+
+    if node_tree.links:
         statements.append("# Links :\n")
-        for link in nt.links:
+        for link in node_tree.links:
             statements.append(get_link_statement(link))
-    
+
     return statements
-        
-def copy_to_text_block(obj):
+
+
+def write_material_to_text_block(obj):
+    """
+    Create or overwrite a text block with the same name as the material
+    Which contains all the necessary statements to duplicate the material
+    """
     if not obj or obj.type not in ('MESH', 'CURVE', 'VOLUME', 'SURFACE', 'FONT', 'META', 'GPENCIL'):
         return
     am = obj.active_material
     if not am or not am.use_nodes:
         return
     statements = serialize_material(am)
-    
+
     text_block = bpy.data.texts.get(am.name)
     if text_block:
         text_block.clear()
@@ -326,15 +345,16 @@ def copy_to_text_block(obj):
     for st in statements:
         text_block.write(st)
         text_block.write("\n")
-    
+
     return text_block
-    
+
 
 def get_socket_index(socket):
     return socket.path_from_id().split(".")[-1].split("[")[-1][:-1]
 
+
 if __name__ == "__main__":
-    text_block = copy_to_text_block(bpy.context.active_object)
+    text_block = write_material_to_text_block(bpy.context.active_object)
     # DEBUG Test if the script works by overwriting the current material :
-#    if text_block:
-#        text_block.as_module()
+    # if text_block:
+    #     text_block.as_module()
